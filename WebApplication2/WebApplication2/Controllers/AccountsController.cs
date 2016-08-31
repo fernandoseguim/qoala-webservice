373fb4df76619245e033fa31432d4123caf0f625
Lucas Gomide de Sousa <RM71058@fiap.com.br>
2016-08-31 18:50:14 -0300

    Add models User and Authorization + Creating routes to register an account, reset password, login and logout

---
+++ WebApplication2/WebApplication2/Controllers/AccountsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class AccountsController : ApiController
+    {
+        [HttpPost]
+        public ControlAccess Register([FromBody] string name, [FromBody] string email, [FromBody] string password)
+        {
+            User user = new User { Name = name, Email = email, Password = password };
+            if(user.register())
+            {
+                ControlAccess controllAccess = new ControlAccess { UserId = user.Id };
+                if(controllAccess.createToken())
+                {
+                    return controllAccess;
+                }
+            }
+            return new ControlAccess { Id = 0, UserId = 0, Token = "abcd" };
+        }
+
+        [HttpPost]
+        public ControlAccess ResetPassword([FromBody] string email)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public ControlAccess Login([FromBody] string email, [FromBody] string password)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public ControlAccess Logout([FromBody] string token)
+        {
+            return null;
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/ControlAccess.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class ControlAccess
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Token { get; set; }
+        public string ExpiresAt { get; set; }
+
+        public bool createToken()
+        {
+            Id = 5;
+            Token = "asdasdsadsadsadsadsadsad";
+            return true;
+        }
+    }
+}
