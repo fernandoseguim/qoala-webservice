97da101f5d8902cda8f1d214c2bc6e0eb97288f1
Gabriel Lucas de Toledo Ribeiro <gabrielltr84@gmail.com>
2016-09-18 11:32:23 -0300

    add assembly version default route, when ended a new build please remember to set this in AssemblyInfo.cs or by application assembly info.

---
+++ QoalaWS/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace QoalaWS.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        [HttpGet]
+        [HttpPost]
+        public IHttpActionResult Index()
+        {
+            var ass = System.Reflection.Assembly.GetExecutingAssembly();
+
+            return Ok(new
+            {
+                version = ass.GetName().Version
+            });
+        }
+    }
+}
