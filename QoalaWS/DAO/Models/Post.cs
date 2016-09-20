de8901efc8c77c39aa996b7ee7b9d8340561cc8d
lucas <lucas@DESKTOP-3H6TG53>
2016-09-20 15:09:17 -0300

    Organizing Exceptions in a folder

---
+++ QoalaWS/DAO/Models/Post.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace QoalaWS.DAO
+{
+    public partial class Post
+    {
+        public decimal? Add(QoalaEntities context)
+        {
+            var outParameter = new ObjectParameter("oUT_ID_POST", typeof(decimal));
+            
+            //if (outParameter.Value == DBNull.Value)
+            //    throw new CreateRecordException();
+
+            //ID_USER = (Decimal)outParameter.Value;
+            //context.Entry(this).State = EntityState.Unchanged;
+            //return ID_USER;
+            return null;
+        }
+    }
+}
