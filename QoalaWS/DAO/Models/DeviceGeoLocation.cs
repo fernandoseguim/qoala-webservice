d9181e889e6e1318584dd6be1e4ac5920b63cd8b
lucas <lucas@DESKTOP-3H6TG53>
2016-09-21 22:12:30 -0300

    Adding model DeviceGeoLocation

---
+++ QoalaWS/DAO/Models/DeviceGeoLocation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.Entity.Core.Objects;
+using QoalaWS.Exceptions;
+using System.Data.Entity;
+
+namespace QoalaWS.DAO
+{
+    public partial class DeviceGeoLocation
+    {
+        public decimal? Add(QoalaEntities context)
+        {
+            var outParameter = new ObjectParameter("OUT_ID_GEO", typeof(decimal));
+            context.SP_INSERT_DEVICE_GEO_LOCATION(
+                ID_DEVICE_GEO_LOCATION, 
+                VERIFIED_AT, 
+                LATITUDE, 
+                LONGITUDE, 
+                outParameter
+            );
+
+            if (outParameter.Value == DBNull.Value)
+                throw new CreateRecordException();
+
+            ID_DEVICE_GEO_LOCATION = (Decimal)outParameter.Value;
+            context.Entry(this).State = EntityState.Unchanged;
+            return ID_DEVICE_GEO_LOCATION;
+        }
+    }
+}
